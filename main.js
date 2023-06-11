f1f1f120913f1483db9420ebf0d683e8bd773391
cloudahn <powguide2@gmail.com>
2023-06-11 19:13:26 +1000

    fifth commint

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ let menus = document.querySelectorAll(".menus button");
 let summary = '';
 let url = '';
 let booksHTML = '';
+let call_index = 0;
 
 //pagination 관련 전역변수
-let total_hits = 0; //검색 된 전체 아이템수
-let total_pages = 0; //전체 페이지 수 ( total_hits / 한페이지당 게시물 수(디폴드 10개) )
-let total_page_groups = 0; // 전체 페이지를 페이지 그룹으로 나눈 그룹 수( total_pages / 페이지네이션 수(디폴트 5개))
-let current_index = 0 ; //현재 보고 있는 페이지 그룹 번호
-let firstPageOfCurrentIndex = 0; //현재 페이지 그룹의 첫 페이지 번호
-let LastPageOfCurrentIndex = 0; //현재 페이지 그룹의 마지막 페이지 번호
+let total_items = 0; //검색 된 전체 건수
+let total_pages = 0; //10개로 나누어진 전체 화면 페이지 수 (/10)
+let total_page_groups = 0; // Pagination 을 위한 전체 페이지 그룹(/5) 
+let current_page_group = 1; //현재 화면의 페이지가 속한 페이지 그룹
+let first_page = 1 // 현재 그룹의 첫번째 페이지
+let last_page = 0; // 현재 그룹의 마지막 페이지
+let current_page = 1;
 
 
 menus.forEach((menu) => menu.addEventListener("click", (event) => getBooksBySubject(event)));
@@ -20,20 +22,24 @@ let searchButton = document.getElementById("search-button");
 const getBooks = async () => {
     try{
       
-        url.searchParams.set("startIndex", current_index); // 원하는 페이지의 index 값을 파라메터로 넣는다. &startIndex=
+        url.searchParams.set("startIndex", call_index); // 원하는 페이지의 index 값을 파라메터로 넣는다. &startIndex=
+
         let response = await fetch(url);
         let data = await response.json();
         console.log(data);
-        total_hits = data.totalItems; //검색 된 총 페이지 수
+        total_items = data.totalItems; //검색 된 전체 건수를 total_page 입력
+        total_pages = Math.ceil(total_items/10);
+        current_page_group = Math.ceil(current_page/5) //현재페이지가 속해있는 페이지 그룹
 
         if(response.status == 200){
             //검색 된 결과가 없을 경우 결과 없음을 출력
-            if(total_hits == 0){
+            if(total_items == 0){
                 throw new Error("검색 된 결과값이 없습니다!!!");
             }else{
                 books = data.items;
-                console.log("data는: ", books);
+                console.log("books data는: ", books);
                 render();
+                pagination();
             }
         }else{
             //검색 창에 아무것도 입력하지 않고 Search 를 한 경우
@@ -130,31 +136,31 @@ const pagination = () => {
 
     let paginationHTML = ''; //페이지 네비게이션을 넣어줄 변수
 
-
-    if(total_hits < 11){ //전체 검색된 건이 11건이 안될 경우, 즉 전체 건이 한페이지에 다 나올 경우
-        firstPageOfCurrentIndex = 1;
-        LastPageOfCurrentIndex = 1;
-        current_index = 0; //인덱스는 0부터 시작하므로
-    } else{
-        total_pages = Math.ceil(total_hits/10); //총 보여줄 화면의 페이지가 몇개 인지 알아내기 (ex. 총 11건이면 보여줄 화면의 수는 2페이지)
-        //total_pages 가 전체 페이지의 Index 수가 됨. 이후 페이징 되는 페이지 번호가 인덱스 -1 로 호출 하면 됨.
-        if (total_pages < 5){ //만약에 보여줄 화면의 수가 5페이지가 안되면 
-            firstPageOfCurrentIndex = 1;
-            LastPageOfCurrentIndex = total_pages; //전체 페이지가 5페이지가 넘지 않으므로 마지막 페이지는 전체페이지의 수가 된다.
-        } else{
-            total_page_groups = Math.ceil(total_pages/5); //총 보여주는 화면의 수를 페이징 숫자인 5로 나눠서 각 5페이지씩 그룹을 만든다.
-            console.log("호출된 페이지 그룹은 : " , pageGroup);
-            if((total_page_groups*5) > total_pages){ //마지막 페이지 그룹의 페이지 수가 5개가 되지 않는다면
-                firstPageOfCurrentIndex = (((total_page_groups-1)*5) + 1) //마지막 페이지의 그룹의 첫번째 페이지 숫자는 이전페이지 그룹의 마지막 숫자 + 1
-                LastPageOfCurrentIndex = total_pages; //마지막 페이지의 그룹의 마지막 페이지 숫자는 전체 페이지의 마지막 번호가 되야 함.
-            } else{ //마지막 페이지 그룹이 아닌 중간 페이지 그룹인 경우
-                firstPageOfCurrentIndex = (total_page_groups * 5) - 4;
-                LastPageOfCurrentIndex = total_page_groups * 5;
-            }
+    //전체 페이지 수가 5개 이하일 경우, 즉 1개의 페이지 그룹만 있는 경우
+    //이경우 first_page = 1이고, last_page = total_pages 가 된다.
+    if (total_pages <= 5){
+        total_page_groups = 1;
+        current_page_group = 1;
+        first_page = 1;
+        last_page = total_pages;
+    } else{ // 페이지 그룹이 최소 2개 이상 있는 경우
+        total_page_groups = Math.ceil(total_pages/5); //페이징할때 5개씩 그룹을 묶을 것임으로
+        if((current_page_group*5) >= total_pages ){ // 즉 현재 페이지 그룹이 마지막 페이지 그룹일 경우
+            first_page = (current_page_group-1)*5 + 1;
+            last_page = total_pages;
+        } else{ //마지막 페이지 그룹이 아닌경우
+            last_page = current_page_group * 5;
+            first_page = last_page - 4;
         }
+        console.log("현재페이지 그룹 : ", current_page_group);
+        console.log("현재 페이지 : ",current_page);
+        console.log("첫번째 페이지 : ", first_page);
+        console.log("마지막 페이지 : ", last_page);
     }
+   
+    /// 실제로 pagination 을 하는 부분
 
-    if (current_index > 0){ //현재 페이지그룹(페이지인덱스)가 첫번째(index 0)가 아니면
+    if (current_page_group > 1){ //현재 페이지그룹이 첫번째 그룹이 아니면
         paginationHTML = ` <li class="page-item">
         <a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(1)">
           <span aria-hidden="true">&laquo;</span>
@@ -165,20 +171,20 @@ const pagination = () => {
       // 마지막 그룹일떄 >> > 이 없어야 한다.
   
         paginationHTML += ` <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(${page-1})">
+        <a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(${current_page-1})">
           <span aria-hidden="true">&lt;</span>
         </a>
       </li>`;
     }
 
-    for (let i = firstPageOfCurrentIndex; i <= LastPageOfCurrentIndex; i++ ){
-        paginationHTML += `<li class="page-item"><a class="page-link ${(current_index+1)==i?"active":""}" href="#">${i}</a></li>`
+    for (let i = first_page; i <= last_page; i++ ){
+        paginationHTML += `<li class="page-item"><a class="page-link ${(current_page)==i?"active":""}" href="#" onclick="moveToPage(${i})">${i}</a></li>`
     }
 
-    if (current_index < (total_page_groups-1)){  //현재 페이지가 마지막 페이지 그룹이 아닐경우
+    if (current_page_group < total_page_groups){  //현재 페이지가 마지막 페이지 그룹이 아닐경우
 
         paginationHTML += `<li class="page-item">
-        <a class="page-link" href="#" aria-label="Next" onclick="moveToPage(${page+1})">
+        <a class="page-link" href="#" aria-label="Next" onclick="moveToPage(${current_page+1})">
           <span aria-hidden="true">&gt;</span>
         </a>
       </li>`;
@@ -196,7 +202,8 @@ const pagination = () => {
 
 const moveToPage = (pageNum) => {
     console.log("moveToPage 호출 됨 + page 값은 :", pageNum);
-    startIndex = pageNum-1; //이동하고자 하는 페이지를 startIndex(Index 이므로 페이지에 -1을 한다)파라메터로 변환해서 넘긴다
+    call_index = pageNum-1; //이동하고자 하는 페이지를 startIndex(Index 이므로 페이지에 -1을 한다)파라메터로 변환해서 넘긴다
+    current_page = pageNum;
     getBooks();
 
 }
